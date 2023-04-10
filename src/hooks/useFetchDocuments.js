291546519b0bb9e4be5943376d3a6078fc35f17e
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2023-04-10 15:40:11 -0300

    dashboard

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -18,12 +18,12 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
 
       try {
         let q;
-
         //busca
-        //dashbaord
         if(search) {
           q = await query(collectionRef, where("tagsArray", "array-contains", search), orderBy("createdAt", "desc"));
-        } else{
+        } else if (uid) {
+          q = await query(collectionRef, where("uid", "==", uid), orderBy("createdAt", "desc"));
+        } else {
           q = await query(collectionRef, orderBy("createdAt", "desc"));
         }
 
@@ -43,7 +43,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
       }
     }
     loadData();
-  }, [docCollection, documents, search, uid, cancelled])
+  }, [docCollection, search, uid, cancelled])
 
   useEffect(() => {
     return () => setCancelled(true);
